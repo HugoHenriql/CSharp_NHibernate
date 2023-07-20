479f11e74be498b0faaa0d6ae15a90fd9d106f9d
Hugo Henrique <hugohenriquelarah@gmail.com>
2023-07-20 16:03:50 -0300

    Adicionando Funcionalidades

--- loja/loja/Entidades/Usuario.cs
+++ loja/loja/Entidades/Usuario.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 namespace loja.Entidades
 {    public class Usuario
     {
+        public virtual int Numero { get; set; }
+        public virtual int Numero { get; set; }
+
+        public virtual int Numero { get; set; }
+        public virtual int Numero { get; set; }
+
+
+
 
         public virtual int Id  { get; set; }
         public virtual string Nome { get; set; }
 
+        public static implicit operator Usuario(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
